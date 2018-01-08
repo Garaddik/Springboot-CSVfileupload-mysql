7005b6fcd73141b487dea8a509f0661ec2cbd9d8
Garaddik <kirangnirmata@gmail.com>
2018-01-08 21:55:00 +0530

    Source code

---
+++ src/main/java/com/mkyong/SpringBootWebApplication.java
@@ -0,0 +1,38 @@
+package com.mkyong;
+
+import org.apache.coyote.http11.AbstractHttp11Protocol;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+
+//https://www.agilegroup.co.jp/technote/springboot-fileupload-error-handling.html
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+    private int maxUploadSizeInMb = 10 * 1024 * 1024; // 10 MB
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+
+    //Tomcat large file upload connection reset
+    //http://www.mkyong.com/spring/spring-file-upload-and-connection-reset-issue/
+    @Bean
+    public TomcatEmbeddedServletContainerFactory tomcatEmbedded() {
+
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+
+        tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+            if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+                //-1 means unlimited
+                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+            }
+        });
+
+        return tomcat;
+
+    }
+
+}
